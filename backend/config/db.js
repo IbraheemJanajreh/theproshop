bf86f9bf1f3e73241779c071d60186a93061df25
IbraheemJanajreh <rf.nadal.22@gmail.com>
2021-08-26 14:04:20 +0300

    prepare for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,18 @@
-import Mongoose from "mongoose";
+import mongoose from "mongoose";
+
 const connectDB = async () => {
   try {
-    const conn = await Mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
+      useNewUrlParser: true,
       useCreateIndex: true,
     });
+
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
-  } catch (err) {
-    console.log(`Error: ${err.message}`.red.underline.bold);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
   }
 };
+
 export default connectDB;
